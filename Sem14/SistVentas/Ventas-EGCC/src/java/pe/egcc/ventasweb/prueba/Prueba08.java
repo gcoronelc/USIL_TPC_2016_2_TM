df2778545f3de833dbbd2c4949e158454660d86f
Gustavo Coronel <gcoronelc@gmail.com>
2016-11-16 12:26:09 -0500

    Sem 14

---
+++ Sem14/SistVentas/Ventas-EGCC/src/java/pe/egcc/ventasweb/prueba/Prueba08.java
@@ -0,0 +1,41 @@
+package pe.egcc.ventasweb.prueba;
+
+import java.util.Calendar;
+import java.util.List;
+import pe.egcc.ventasweb.model.Detalle;
+import pe.egcc.ventasweb.model.Promocion;
+import pe.egcc.ventasweb.model.Venta;
+import pe.egcc.ventasweb.service.espec.PromocionServiceEspec;
+import pe.egcc.ventasweb.service.espec.VentaServiceEspec;
+import pe.egcc.ventasweb.service.impl.PromocionServiceImpl;
+import pe.egcc.ventasweb.service.impl.VentaServiceImpl;
+
+/**
+ *
+ * @author Eric Gustavo Coronel Castillo
+ * @blog www.desarrollasoftware.com
+ * @email egcc.usil@gmail.com
+ */
+public class Prueba08 {
+
+  public static void main(String[] args) {
+    
+    // Creando una venta
+    Venta venta = new Venta();
+    venta.setCliente("Gustavo");
+    venta.setIdemp(1002);
+    venta.setFecha(Calendar.getInstance().getTime());
+    venta.addItem(new Detalle(0, 0, 1, "Papas", 5, 3, 1, 5));
+    venta.addItem(new Detalle(0, 0, 2, "Camote", 15, 4, 2, 30));
+
+    try {
+      // Grabar venta
+      VentaServiceEspec service = new VentaServiceImpl();
+      service.grabar(venta);
+      System.out.println("idventa = " + venta.getIdventa());
+    } catch (Exception e) {
+      System.err.println("Error: " + e.getMessage());
+    }
+
+  }
+}
